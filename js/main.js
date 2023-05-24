df821536826e601195258688db502db78f45e090
sebastian nunez <edgardo.-sn@hotmail.com>
2023-05-24 20:55:16 -0300

    modificar la leyenda del color de la pagina

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-function changeBackground()
+document.getElementById("color").addEventListener("click",changeBackground);
+function changeBackground() {
+  const symbols = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+  ];
+
+let color = "#";
+for (let i = 0; i < 6; i++) {
+  color += symbols[Math.floor(Math.random() * 16)];
+}
+document.body.style.background=color;
+document.getElementById("hexa").innerHTML=color;
+}
